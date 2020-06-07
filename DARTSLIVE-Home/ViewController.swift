565bb17e97e78a93d084f8f7abe9a10420b755ce
赤迫亮太 <ichimots@gmail.com>
2020-06-07 16:14:36 +0900

    init

---
+++ DARTSLIVE-Home/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DARTSLIVE-Home
+//
+//  Created by 赤迫亮太 on 2020/06/07.
+//  Copyright © 2020 赤迫亮太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
